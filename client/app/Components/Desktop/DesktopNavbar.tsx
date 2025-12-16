5d6ab6dd78177a0a2b869b08b21b9a571e109e00
Harsh <pandeygautam039@gmail.com>
2025-12-17 04:47:57 +0530

    Added Pure Animations Logic to the COde

--- client/app/Components/Desktop/DesktopNavbar.tsx
+++ client/app/Components/Desktop/DesktopNavbar.tsx
@@ -1,12 +1,44 @@
 "use client";
-import Image from "next/image";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import { FaWindows } from "react-icons/fa6";
 import { FiSearch, FiUser, FiWifi } from "react-icons/fi";
 import { Tooltip } from "react-tooltip";
 
 const DesktopNavbar = () => {
+  useGSAP(() => {
+    const tl = gsap.timeline();
+
+    tl.from("#DeskNav", {
+      y: -20,
+      opacity: 0,
+      duration: 0.95,
+      ease: "back.out(1.7)",
+      delay: 0.4,
+    });
+
+    tl.from("#DeskLeftNavItems", {
+      y: -20,
+      opacity: 0,
+      duration: 0.2,
+      ease: "power2.out",
+      stagger: 0.06,
+    });
+
+    tl.from("#DeskRightNavItems", {
+      y: -20,
+      opacity: 0,
+      duration: 0.2,
+      ease: "power2.out",
+      stagger: 0.02,
+    });
+  }, []);
+
   return (
-    <div className="w-full absolute top-0 z-60 bg-gray-900/40 backdrop-blur-md border-b border-gray-700 font-semibold py-4 px-12">
+    <div
+      className="w-full absolute top-0 z-60 bg-gray-900/40 backdrop-blur-md border-b border-gray-700 font-semibold py-4 px-12"
+      id="DeskNav"
+    >
       <div className="h-full w-full flex justify-between items-center pt-2">
         <div className="flex items-center gap-4 text-white">
           <FiUser className="h-4 w-4" />
@@ -18,6 +50,7 @@ const DesktopNavbar = () => {
                 data-tooltip-id="navItems-tooltip"
                 data-tooltip-content={item}
                 className="cursor-pointer underline-offset-2 hover:underline"
+                id="DeskLeftNavItems"
               >
                 {item}
               </button>
@@ -33,9 +66,10 @@ const DesktopNavbar = () => {
           ].map(({ Icon, name }, index) => (
             <Icon
               key={index}
-              className="cursor-pointer w-4 h-4"
+              className="cursor-pointer w-4 h-4 outline-none"
               data-tooltip-id="navItems-tooltip"
               data-tooltip-content={name}
+              id="DeskLeftNavItems"
             />
           ))}
           <div
