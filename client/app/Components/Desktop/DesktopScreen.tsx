34c537e4c316e6edcbaa73cc2e4009ecb783fb35
Harsh <pandeygautam039@gmail.com>
2025-12-17 01:54:19 +0530

    Added a Text Animation at DestopScreen

--- client/app/Components/Desktop/DesktopScreen.tsx
+++ client/app/Components/Desktop/DesktopScreen.tsx
@@ -1,13 +1,14 @@
 "use client";
 import Image from "next/image";
 import DesktopNavbar from "./DesktopNavbar";
+import DesktopText from "./DesktopText";
 
 const DesktopScreen = () => {
   return (
     <>
       <div className="h-screen w-full overflow-hidden">
         <Image
-          src="/images/wallpaper.png"
+          src="/images/wallpaper.jpg"
           alt="Desktop Background"
           fill
           loading="lazy"
@@ -16,6 +17,11 @@ const DesktopScreen = () => {
         />
         {/* Navbar */}
         <DesktopNavbar />
+
+        {/* Animated Text Area */}
+        <div className="absolute top-1/2 -translate-x-1/2 -translate-y-1/2 left-[51.6%] text-center text-white font-bold">
+          <DesktopText />
+        </div>
       </div>
     </>
   );
---
+++ client/app/Components/Desktop/DesktopText.tsx
@@ -0,0 +1,97 @@
+import { useRef } from "react";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+const RenderText = (
+  text: string,
+  className: string,
+  baseWeight: number = 400
+) => {
+  return [...text].map((char, index) => (
+    <span
+      key={index}
+      className={className}
+      style={
+        {
+          "--wght": baseWeight,
+          fontVariationSettings: `'wght' var(--wght)`,
+          display: "inline-block",
+        } as React.CSSProperties
+      }
+    >
+      {char === " " ? "\u00A0" : char}
+    </span>
+  ));
+};
+
+const FONT_WEIGHTS: any = {
+  subtitle: { min: 100, max: 400, default: 100 },
+  title: { min: 400, max: 900, default: 400 },
+};
+
+const setupTextHover = (container: any, type: any) => {
+  if (!container) return;
+
+  const letters = container.querySelectorAll("span");
+  const { min, max, default: base } = FONT_WEIGHTS[type];
+
+  const animatedText = (letter: any, weight: any, duration: any = 0.32) => {
+    return gsap.to(letter, {
+      "--wght": weight,
+      duration,
+      ease: "power2.out",
+    });
+  };
+
+  const handleMouseEnter = (e: any) => {
+    const { left } = container.getBoundingClientRect();
+    const mouseX = e.clientX - left;
+
+    letters.forEach((letter: any) => {
+      const { left: l, width: w } = letter.getBoundingClientRect();
+      const distance = Math.abs(mouseX - (l - left + w / 2));
+      const intensity = Math.exp(-(distance ** 2) / 2000);
+
+      animatedText(letter, min + (max - min) * intensity);
+    });
+  };
+
+  const handleMouseLeave = () => {
+    letters.forEach((letter: any) => {
+      animatedText(letter, base, 0.3);
+    });
+  };
+
+  container.addEventListener("mousemove", handleMouseEnter);
+  container.addEventListener("mouseleave", handleMouseLeave);
+};
+
+const DesktopText = () => {
+  const TitleRef = useRef(null);
+  const SubTitleRef = useRef(null);
+
+  useGSAP(() => {
+    setupTextHover(TitleRef.current, "title");
+    setupTextHover(SubTitleRef.current, "subtitle");
+  }, []);
+
+  return (
+    <div
+      id="text-animation-desktop"
+      className="cursor-default select-none pointer-events-auto"
+    >
+      <p ref={SubTitleRef}>
+        {" "}
+        {RenderText(
+          "Hello! , I'm Harsh Pandey, a Full Stack Web Developer.",
+          "text-[24px] font-ligh tracking-tight"
+        )}
+      </p>
+      <h1 ref={TitleRef}>
+        {RenderText("portfolio.", "text-9xl italic mt-7 font-thin")}
+      </h1>
+    </div>
+  );
+};
+
+export default DesktopText;
